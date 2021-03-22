0b1403da6a44a57936116ff6b16de1729bcb7f5f
Gabe Kirkley <gdgkirkley@gmail.com>
2021-03-21 22:08:57 -0700

    Move regenerator runtime to index

--- dev.js
+++ dev.js
@@ -1,5 +1,4 @@
 (async () => {
-  require("regenerator-runtime/runtime");
   require("@babel/register")({ extensions: [".js", ".ts"] });
   require("./src/index.ts");
 })();
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 if (process.env.NODE_ENV === "production") {
+  require("regenerator-runtime/runtime");
   require("./dist/index.js");
 } else {
   require("nodemon")({ script: "dev.js" });
