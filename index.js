66094f831093a2a9a15478ffcddbef4ae7b28410
Gabe Kirkley <gdgkirkley@gmail.com>
2021-03-21 22:13:27 -0700

    Add babel plugin for runtime

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 if (process.env.NODE_ENV === "production") {
-  require("regenerator-runtime/runtime");
   require("./dist/index.js");
 } else {
   require("nodemon")({ script: "dev.js" });
