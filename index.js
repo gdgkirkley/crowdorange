202f10aab7a38cdf116b89dc8b9aeb0facbadd16
Gabe Kirkley <gdgkirkley@gmail.com>
2021-03-21 21:24:12 -0700

    Attempt build before tsx

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-require('dotenv').config()
-if (process.env.NODE_ENV === 'production') {
-  require('./dist')
+require("dotenv").config();
+if (process.env.NODE_ENV === "production") {
+  require("./dist/index.js");
 } else {
-  require('nodemon')({script: 'dev.js'})
+  require("nodemon")({ script: "dev.js" });
 }
